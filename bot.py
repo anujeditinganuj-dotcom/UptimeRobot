e5b0f6e6881c42cb79676e22162caeeed9dc5dbe
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-02 03:31:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,25 +7,25 @@ import sys
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 import asyncio
 
-API_ID = int(os.environ.get("API_ID", "1"))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "34446649"))
+API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8040657764:AAGFwTyS5ds9KsNyOtwaBLTnyb--oaCV7zE")
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "UptimeRobot")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Anujedit")
 
-OWNER = int(os.environ.get("OWNER", "8367080346"))
-ADMINS = os.environ.get("ADMINS", "1335306418")
+OWNER = int(os.environ.get("OWNER", "7892805795"))
+ADMINS = os.environ.get("ADMINS", "7892805795")
 ADMINS = [int(admin) for admin in (ADMINS).split(" ")]
 ADMINS.append(OWNER)
 
-MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "BOTSKINGDOMSGROUP")
+MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "TeraBox_Support_Anuj_Bot")
 FORCE_SUB_TEXT = os.environ.get(
     "FORCE_SUB_TEXT",
     """<b><i>❗️ You must join our channel before using this feature:</i></b>"""
 )
 
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "𝕸𝖆𝖎𝖓 𝕮𝖍𝖆𝖓𝖓𝖊𝖑: ")
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "TeraBox_Support_Anuj_Bot")
 
 SETTINGS = {
     "ADMINS": ADMINS,
@@ -76,13 +76,13 @@ def get_logger(name: str = "app") -> logging.Logger:
   return logger
 
 
-log = get_logger(__name__)
+log = get_logger(name)
 
 
 class UpTimeRoBot(Client):
 
-  def __init__(self):
-    super().__init__("bot",
+  def init(self):
+    super().init("bot",
                      api_id=API_ID,
                      api_hash=API_HASH,
                      bot_token=BOT_TOKEN,
@@ -119,7 +119,7 @@ class UpTimeRoBot(Client):
     message_task = None
     callback_task = None
 
-    try:
+try:
       # Create tasks
       message_task = asyncio.create_task(
           self.listen(user_id=user_id, filters=msg_filter,
@@ -193,7 +193,8 @@ PICS = (
     "https://img1.teletype.in/files/0a/1b/0a1b5f17-095c-4826-84c8-39a8b9b9deef.jpeg",
     "https://img4.teletype.in/files/f5/94/f594fbe2-b52d-489a-86c9-23b2f2dbe4d7.jpeg",
     "https://img3.teletype.in/files/e3/76/e376be29-065b-4c1a-986d-aba69d08208f.jpeg",
-    "https://img1.teletype.in/files/8f/e6/8fe67878-43a3-4b3d-851f-63727a6a2b0b.jpeg",
+
+"https://img1.teletype.in/files/8f/e6/8fe67878-43a3-4b3d-851f-63727a6a2b0b.jpeg",
     "https://img2.teletype.in/files/1a/d3/1ad3fa24-c3bf-4ca8-a7ef-a79286b1e37c.jpeg",
     "https://img1.teletype.in/files/80/1a/801a77ad-bf05-4d7a-96c9-2b1cde09d04f.jpeg",
     "https://img4.teletype.in/files/f4/b0/f4b007ec-fc8c-49fd-a1fb-b0d02985120a.jpeg",
@@ -228,7 +229,8 @@ PICS = (
     "https://ik.imagekit.io/jbxs2z512/21418.jpg?updatedAt=1751107452919",
     "https://ik.imagekit.io/jbxs2z512/mythical-dragon-beast-anime-style_23-2151112835.jpg?updatedAt=1751107574210",
     "https://ik.imagekit.io/jbxs2z512/halloween-scene-illustration-anime-style_23-2151794288.jpg?updatedAt=1751107676806",
-    "https://ik.imagekit.io/jbxs2z512/5823589-2920x1640-desktop-hd-boy-programmer-wallpaper-image.jpg_id=1726666227?updatedAt=1751107911063",
+
+"https://ik.imagekit.io/jbxs2z512/5823589-2920x1640-desktop-hd-boy-programmer-wallpaper-image.jpg_id=1726666227?updatedAt=1751107911063",
     "https://ik.imagekit.io/jbxs2z512/thumbbig-1345576.webp?updatedAt=1751108065802",
     "https://ik.imagekit.io/jbxs2z512/thumb-440-1340473.webp?updatedAt=1751108159970",
     "https://ik.imagekit.io/jbxs2z512/wp3084738.jpg?updatedAt=1751108326075",
@@ -267,7 +269,8 @@ PICS = (
     "https://ik.imagekit.io/jbxs2z512/wp15082622.webp?updatedAt=1751628578344",
     "https://ik.imagekit.io/jbxs2z512/e618b0d2a9518326722d6483569a94c6.jpg?updatedAt=1757316947948",
     "https://ik.imagekit.io/jbxs2z512/6222c23f673d9d6b86cf759a440935d9.jpg?updatedAt=1757317031249",
-    "https://ik.imagekit.io/jbxs2z512/b322fad3ae2e550b5e9a0cb1d2c78311.jpg?updatedAt=1757317116796",
+
+"https://ik.imagekit.io/jbxs2z512/b322fad3ae2e550b5e9a0cb1d2c78311.jpg?updatedAt=1757317116796",
     "https://ik.imagekit.io/jbxs2z512/0a7f44752689424fd901b220aa5c5516.jpg?updatedAt=1757317197691",
     "https://ik.imagekit.io/jbxs2z512/c4567ce2326aa09a4b33b0ff21e9d1ef.jpg?updatedAt=1757317301147",
     "https://ik.imagekit.io/jbxs2z512/a6a2b0fe748ec78ead3c3ac85b1472a8.jpg?updatedAt=1757317366150",
